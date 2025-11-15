82543630f83648cd7d44c6b93bbd95fd94777311
HyuDev7 <hyu.df7@gmail.com>
2025-11-15 21:46:46 +0900

    add: added csv util dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,6 +24,11 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+	// build.gradle.kts の dependencies ブロックに追加
+	implementation("org.apache.commons:commons-csv:1.10.0")
+
+
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
